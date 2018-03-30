5ad6b067738830f8b91e57df4569aa902f252f99
Sixclones <roch.bouchayer@gmail.com>
2018-03-31 00:40:50 +0200

    setup, scripts for symlinks & vim

---
+++ scripts/create-symlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sf ~/.dotfiles/.vimrc ~
+ln -sf ~/.dotfiles/.vimcmd ~
+ln -sf ~/.dotfiles/.zshrc ~
+
+echo "Symlinks created"
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh ./scripts/create-symlinks.sh
